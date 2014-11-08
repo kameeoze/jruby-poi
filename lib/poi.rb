c0d68ff32c9d19fa28d44d0a9f0fd3f3c6c07314
Angus Miller <all4miller@gmail.com>
2014-11-08 09:39:01 +0200

    upgrade to POI 3.10

--- lib/poi.rb
+++ lib/poi.rb
@@ -2,11 +2,11 @@ JRUBY_POI_LIB_PATH=File.expand_path(File.dirname(__FILE__))
 
 # Java
 require 'java'
-require File.join(JRUBY_POI_LIB_PATH, 'poi-3.8-20120326.jar')
-require File.join(JRUBY_POI_LIB_PATH, 'poi-ooxml-3.8-20120326.jar')
-require File.join(JRUBY_POI_LIB_PATH, 'poi-ooxml-schemas-3.8-20120326.jar')
-require File.join(JRUBY_POI_LIB_PATH, 'poi-scratchpad-3.8-20120326.jar')
-require File.join(JRUBY_POI_LIB_PATH, 'ooxml-lib', 'xmlbeans-2.3.0.jar')
+require File.join(JRUBY_POI_LIB_PATH, 'poi-3.10.1-20140818.jar')
+require File.join(JRUBY_POI_LIB_PATH, 'poi-ooxml-3.10.1-20140818.jar')
+require File.join(JRUBY_POI_LIB_PATH, 'poi-ooxml-schemas-3.10.1-20140818.jar')
+require File.join(JRUBY_POI_LIB_PATH, 'poi-scratchpad-3.10.1-20140818.jar')
+require File.join(JRUBY_POI_LIB_PATH, 'ooxml-lib', 'xmlbeans-2.6.0.jar')
 require File.join(JRUBY_POI_LIB_PATH, 'ooxml-lib', 'dom4j-1.6.1.jar')
 require File.join(JRUBY_POI_LIB_PATH, 'ooxml-lib', 'stax-api-1.0.1.jar')
 
--- lib/poi/workbook/area.rb
+++ lib/poi/workbook/area.rb
@@ -3,13 +3,13 @@ module POI
     def initialize reference
       @ref = reference
     end
-    
+
     def in workbook
       if single_cell_reference?
         ref = area.all_referenced_cells.first
         return [workbook.single_cell(ref)]
       end
-      
+
       begin
         by_column = {}
         # refs = area.all_referenced_cells
@@ -20,7 +20,7 @@ module POI
         #       first = workbook.worksheets[cell_ref.sheet_name].first_row
         #       last  = workbook.worksheets[cell_ref.sheet_name].last_row
         #       next unless cell_ref.row >= first && cell_ref.row <= last
-        # 
+        #
         #       # ref = POI::CELL_REF.new(c.format_as_string)
         #       cell = workbook.single_cell cell_ref
         #       (by_column[cell_ref.cell_ref_parts.collect.last] ||= []) << cell
@@ -32,16 +32,16 @@ module POI
           first = workbook.worksheets[cell_ref.sheet_name].first_row
           last  = workbook.worksheets[cell_ref.sheet_name].last_row
           next unless cell_ref.row >= first && cell_ref.row <= last
-        
+
           # ref = POI::CELL_REF.new(c.format_as_string)
           cell = workbook.single_cell cell_ref
           (by_column[cell_ref.cell_ref_parts.to_a.last] ||= []) << cell
         end
-                
+
         by_column.each do |key, cells|
           by_column[key] = cells.compact
         end
-        
+
         if by_column.length == 1
           by_column.values.flatten
         else
@@ -51,31 +51,33 @@ module POI
         []
       end
     end
-    
+
     def single_cell_reference?
       area.single_cell? #@ref == getFirstCell.formatAsString rescue false
     end
-    
+
     private
       def area
         @area ||= new_area_reference
       end
-      
+
       def new_area_reference
         begin
+          return column_reference
+        rescue
           return POI::AREA_REF.new(@ref)
         rescue
-          # not a valid reference, so proceed through to see if it's a column-based reference
+          raise "could not determine area reference for #{@ref}: #{$!.message}"
         end
+      end
+
+      def column_reference
         sheet_parts = @ref.split('!')
         area_parts  = sheet_parts.last.split(':')
         area_start  = "#{sheet_parts.first}!#{area_parts.first}"
         area_end    = area_parts.last
-        begin
-          POI::AREA_REF.getWholeColumn(area_start, area_end)
-        rescue
-          raise "could not determine area reference for #{@ref}: #{$!.message}"
-        end
+
+        POI::AREA_REF.getWholeColumn(area_start, area_end)
       end
   end
 end
